63bb571bc9d6373ba43852593de672e332702515
kc9ryt <hawksj@troop359bsa.org>
2017-12-01 13:08:09 -0500

    Commit message

--- LHS-Athletics/simpleweather/js/index.js
+++ LHS-Athletics/simpleweather/js/index.js
@@ -1,11 +1,8 @@
 // v3.1.0
 //Docs at http://simpleweatherjs.com
-$(document).ready(function() {  
-  getWeather(); //Get the initial weather.
-  setInterval(getWeather, 120000); //Update the weather every 2 minutes.
-});
 
-function getWeather() {
+
+$(document).ready(function() { 
   $.simpleWeather({
     location: 'Lebanon, IN',
     unit: 'f',
@@ -13,8 +10,11 @@ function getWeather() {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
-      //html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-  
+      //html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li>';
+      html += '<br>'
+      html += '<li>'+weather.high+' / '+weather.low+'</li>';
+      html += '</ul>'
+      html += '<h6>High/Low</h6>'
       $("#weather").html(html);
     },
     error: function(error) {
