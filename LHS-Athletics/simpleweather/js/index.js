5d506e29dfa4319bd1821df6b73b035aefba146e
kc9ryt <hawksj@troop359bsa.org>
2017-12-01 13:01:11 -0500

    Commit message

--- LHS-Athletics/simpleweather/js/index.js
+++ LHS-Athletics/simpleweather/js/index.js
@@ -1,9 +1,13 @@
 // v3.1.0
 //Docs at http://simpleweatherjs.com
-$(document).ready(function() {
+$(document).ready(function() {  
+  getWeather(); //Get the initial weather.
+  setInterval(getWeather, 120000); //Update the weather every 2 minutes.
+});
+
+function getWeather() {
   $.simpleWeather({
     location: 'Lebanon, IN',
-    woeid: '',
     unit: 'f',
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
