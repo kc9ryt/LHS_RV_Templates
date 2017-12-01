39642ee719dbf7d9e76ad87ad5f79597feb4960d
kc9ryt <hawksj@troop359bsa.org>
2017-12-01 13:46:12 -0500

    Commit message

--- LHS-Athletics/simpleweather/js/index.js
+++ LHS-Athletics/simpleweather/js/index.js
@@ -8,14 +8,10 @@ $(document).ready(function() {
     unit: 'f',
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      //html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li>';
-      html += '<br>';
-      html += '<br>';
-      html += '<li>'+weather.high+' / '+weather.low+'</li>';
-      html += '</ul>'
-      html += '<h6>High/Low</h6>'
+      html += '<h1> Todays High</h1>'
+      html += '<p>'+weather.high+'</p>';
+      html += '<h1> Todays Low</h1>'
+      html += '<p>'+weather.low+'</p>';
       $("#weather").html(html);
     },
     error: function(error) {
